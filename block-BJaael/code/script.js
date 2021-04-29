44e92b9d7889e1537235943be01eea66b92a617f
sohail shaik <sohailshaik765@gmail.com>
2021-04-29 12:22:52 +0530

    Disco Grid

--- block-BJaael/code/script.js
+++ block-BJaael/code/script.js
@@ -1,32 +1,47 @@
-let table = document.querySelector('table');
-// let tbody = document.querySelector('tbody');
-// let tr = document.querySelector('tr')
-// let td = document.querySelectorAll('td')
-// tr.append(td)
-// tbody.append(tr)
-// table.append(tbody)
-
-
-function handleTd(){
-    let randomColor = generateRandomColor();
-    table.style.background = randomColor
-}
+let allBoxes = document.querySelector('.boxes');
 
 function generateRandomColor(){
-    // let x = Math.floor(Math.random() * 256);
-    // let y = Math.floor(Math.random() * 256);
-    // let z = Math.floor(Math.random() * 256);
-
-    // let bgColor = 'rgb(' + x + ',' + y + ',' + z  + ')';
-    // table.style.background = bgColor;
     let hexCharacters = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'];
 
     let color = '#';
     for(let i = 0 ; i < 6 ; i++){
-        let randomNumber = Math.floor(Math.random() * 16); 
+        let randomNumber = getRandomNumber(16); 
         color += hexCharacters[randomNumber];
     }
     return color;
 }
 
-table.addEventListener('click', handleTd);
+function getRandomNumber(max){
+    return Math.floor(Math.random() * max);
+}
+
+// Create 500 boxes
+
+for(let i = 0 ; i < 500 ; i++){
+    let div = document.createElement('div');
+    div.classList.add('box');
+    // div.style.background = generateRandomColor();
+
+    let h3 = document.createElement('h3');
+    h3.classList.add('h3')
+    let randomNum = getRandomNumber(500);
+    h3.innerText = randomNum;
+
+    div.append(h3)
+    allBoxes.append(div)
+
+}
+// Add an event of mousemove to change each random number and color
+let eachBox = document.querySelectorAll('.box')
+let changeNum = document.querySelectorAll('.h3')
+function handleMouse(){
+    eachBox.forEach((box) => {
+        box.style.background = generateRandomColor();
+    })
+    changeNum.forEach((num) => {
+        num.innerHTML = getRandomNumber(500);
+    })
+}
+
+
+allBoxes.addEventListener('mousemove', handleMouse)
